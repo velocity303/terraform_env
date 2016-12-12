c023e1c0934196d09c4a4f187f2fe419a86b2a9b
James E. Jones <james.jones@puppet.com>
2016-12-12 17:31:52 -0600

    add chicago and portland datacenter changes for app server

--- chicago.tf
+++ chicago.tf
@@ -2,11 +2,25 @@ resource "openstack_compute_floatingip_v2" "fileserverchiip" {
   pool = "ext-net-pdx1-opdx1"
 }
 
+resource "openstack_compute_floatingip_v2" "myappchiip" {
+  pool = "ext-net-pdx1-opdx1"
+}
+
 data "template_file" "init_fileserverchi" {
     template = "${file("bootstrap/bootstrap_agent.tpl")}"
     vars {
         role            = "fileserver"
         name            = "fileserverchi.chicago.lab"
+        master_name     = "${openstack_compute_instance_v2.puppet.name}"
+        masterip        = "${openstack_compute_instance_v2.puppet.network.0.fixed_ip_v4}"
+    }
+}
+
+data "template_file" "init_myappchi" {
+    template = "${file("bootstrap/bootstrap_agent.tpl")}"
+    vars {
+        role            = "myapp"
+        name            = "myappchi.chicago.lab"
         master_name     = "${openstack_compute_instance_v2.puppet.name}"
         masterip        = "${openstack_compute_instance_v2.puppet.network.0.fixed_ip_v4}"
     }
@@ -27,5 +41,22 @@ resource "openstack_compute_instance_v2" "fileserverchi" {
   }
 
   user_data = "${data.template_file.init_fileserverchi.rendered}"
+}
+
+resource "openstack_compute_instance_v2" "myappchi" {
+  name              = "myappchi.chicago.lab"
+  image_name        = "centos_7_x86_64"
+  availability_zone = "opdx1"
+  flavor_name       = "d1.medium"
+  key_pair          = "${var.openstack_keypair}"
+  security_groups   = ["default", "sg0"]
+
+  network {
+    name = "network1"
+    floating_ip = "${openstack_compute_floatingip_v2.myappchiip.address}"
+    access_network = true
+  }
+
+  user_data = "${data.template_file.init_myappchi.rendered}"
 }
 
--- portland.tf
+++ portland.tf
@@ -2,11 +2,25 @@ resource "openstack_compute_floatingip_v2" "fileserverpdxip" {
   pool = "ext-net-pdx1-opdx1"
 }
 
+resource "openstack_compute_floatingip_v2" "myapppdx" {
+  pool = "ext-net-pdx1-opdx1"
+}
+
 data "template_file" "init_fileserverpdx" {
     template = "${file("bootstrap/bootstrap_agent.tpl")}"
     vars {
         role            = "fileserver"
         name            = "fileserverpdx.portland.lab"
+        master_name     = "${openstack_compute_instance_v2.puppet.name}"
+        masterip        = "${openstack_compute_instance_v2.puppet.network.0.fixed_ip_v4}"
+    }
+}
+
+data "template_file" "init_myapppdx" {
+    template = "${file("bootstrap/bootstrap_agent.tpl")}"
+    vars {
+        role            = "myapp"
+        name            = "myapppdx.portland.lab"
         master_name     = "${openstack_compute_instance_v2.puppet.name}"
         masterip        = "${openstack_compute_instance_v2.puppet.network.0.fixed_ip_v4}"
     }
@@ -27,5 +41,22 @@ resource "openstack_compute_instance_v2" "fileserverpdx" {
   }
 
   user_data = "${data.template_file.init_fileserverpdx.rendered}"
+}
+
+resource "openstack_compute_instance_v2" "myapppdx" {
+  name              = "myapppdx.portland.lab"
+  image_name        = "centos_7_x86_64"
+  availability_zone = "opdx1"
+  flavor_name       = "d1.medium"
+  key_pair          = "${var.openstack_keypair}"
+  security_groups   = ["default", "sg0"]
+
+  network {
+    name = "network1"
+    floating_ip = "${openstack_compute_floatingip_v2.myapppdx.address}"
+    access_network = true
+  }
+
+  user_data = "${data.template_file.init_myapppdx.rendered}"
 }
 
