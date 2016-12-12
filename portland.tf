368f6b65507718fcb05d0be0f6f7a56747bc94fe
James E. Jones <james.jones@puppet.com>
2016-12-12 15:56:03 -0600

    update datacenters

---
+++ portland.tf
@@ -0,0 +1,31 @@
+resource "openstack_compute_floatingip_v2" "fileserverpdxip" {
+  pool = "ext-net-pdx1-opdx1"
+}
+
+data "template_file" "init_fileserverpdx" {
+    template = "${file("bootstrap/bootstrap_agent.tpl")}"
+    vars {
+        role            = "fileserver"
+        name            = "fileserver.portland.lab"
+        master_name     = "${openstack_compute_instance_v2.puppet.name}"
+        masterip        = "${openstack_compute_instance_v2.puppet.network.0.fixed_ip_v4}"
+    }
+}
+
+resource "openstack_compute_instance_v2" "fileserverpdx" {
+  name              = "fileserverpdx.portland.lab"
+  image_name        = "centos_7_x86_64"
+  availability_zone = "opdx1"
+  flavor_name       = "d1.medium"
+  key_pair          = "${var.openstack_keypair}"
+  security_groups   = ["default", "sg0"]
+
+  network {
+    name = "network2"
+    floating_ip = "${openstack_compute_floatingip_v2.fileserverpdxip.address}"
+    access_network = true
+  }
+
+  user_data = "${data.template_file.init_fileserverpdx.rendered}"
+}
+
